fb32084593d3e91bbf8492016ef9d05bdc2b915e
Duru Favour <favourduru24@gmail.com>
2025-01-19 19:26:12 -0800

    First Commit

---
+++ src/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div>
+            <h1>iMovies</h1>
+        </div>
+
+        <div>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
